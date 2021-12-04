1bf79e534ef2eeecbf6ae7de03e7bab1c5474e66
OpportunV <RsTGear@gmail.com>
2021-12-04 21:04:59 +0300

    Initial commit

---
+++ AdventOfCode2017/Days/Day1.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using AdventOfCode2017.Helpers;
+
+namespace AdventOfCode2017.Days
+{
+    public static class Day1
+    {
+        private static readonly string _inputPath = Path.Combine("input",
+            $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType?.Name}.txt");
+        
+        public static object Part1()
+        {
+            var lines = Helper.GetInput(_inputPath);
+
+            return -1;
+        }
+        
+        public static object Part2()
+        {
+            var lines = Helper.GetInput(_inputPath);
+
+            return -1;
+        }
+    }
+}
---
+++ AdventOfCode2017/Days/Day3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using AdventOfCode2017.Helpers;
+
+namespace AdventOfCode2017.Days
+{
+    public static class Day3
+    {
+        private static readonly string _inputPath = Path.Combine("input",
+            $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType?.Name}.txt");
+        
+        public static object Part1()
+        {
+            var lines = Helper.GetInput(_inputPath);
+
+            return -1;
+        }
+        
+        public static object Part2()
+        {
+            var lines = Helper.GetInput(_inputPath);
+            
+            return -1;
+        }
+    }
+}
