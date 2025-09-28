bb95070ddf0c7c562dfdf7c0ad09fe724df60391
OpportunV <RsTGear@gmail.com>
2025-09-28 13:57:27 +0300

    Day 7

--- AdventOfCode2017/Days/Day7.cs
+++ AdventOfCode2017/Days/Day7.cs
@@ -1,26 +1,91 @@
-using System.IO;
-using AdventOfCode2017.Helpers;
+using Common.Extensions;
 
-namespace AdventOfCode2017.Days
+namespace AdventOfCode2017.Days;
+
+public class Day7 : Day
 {
-    public static class Day7
-    {
-        private static readonly string _inputPath = Path.Combine("input",
-            $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType?.Name}.txt");
-        
-        public static object Part1()
-        {
-            var lines = Helper.GetInput(_inputPath);
+    private string _root = null!;
+    private readonly Dictionary<string, List<string>> _progChildren = new();
+    private readonly Dictionary<string, int> _progWeight = new();
 
-            return -1;
-        }
-        
-        public static object Part2()
+    public override object Part1()
+    {
+        var lines = GetInput();
+        var supported = new HashSet<string>();
+        var all = new HashSet<string>();
+        foreach (var line in lines)
         {
-            var lines = Helper.GetInput(_inputPath);
-            
-            return -1;
+            var programs = line.GetWords();
+            all.Add(programs[0]);
+            if (programs.Count > 2)
+            {
+                supported.UnionWith(programs[2..]);
+            }
         }
+
+        all.ExceptWith(supported);
+        _root = all.First();
+
+        return _root;
+    }
+
+    public override object Part2()
+    {
+        var lines = GetInput();
+
+        foreach (var line in lines)
+        {
+            var programs = line.GetWords();
+            _progWeight[programs[0]] = int.Parse(programs[1]);
+            if (programs.Count > 2)
+            {
+                _progChildren[programs[0]] = programs[2..];
+            }
+        }
+
+        var candidate = _root;
+        var previousTargetWeight = 0;
+
+        while (_progChildren.TryGetValue(candidate, out var children))
+        {
+            var weights = children.Select(GetTotalWeight).ToList();
+
+            var unique = weights
+                .GroupBy(weight => weight)
+                .FirstOrDefault(group => group.Count() == 1);
+
+            if (unique != null)
+            {
+                candidate = children[weights.IndexOf(unique.Key)];
+                previousTargetWeight = weights.First(weight => weight != unique.Key);
+            }
+            else
+            {
+                previousTargetWeight -= weights.Sum();
+                break;
+            }
+        }
+
+        return previousTargetWeight;
+    }
+
+    private int GetTotalWeight(string prog)
+    {
+        var total = 0;
+        var toVisit = new Queue<string>();
+        toVisit.Enqueue(prog);
+        while (toVisit.TryDequeue(out var cur))
+        {
+            total += _progWeight[cur];
+            if (_progChildren.TryGetValue(cur, out var children))
+            {
+                foreach (var child in children)
+                {
+                    toVisit.Enqueue(child);
+                }
+            }
+        }
+
+        return total;
     }
 }
-
