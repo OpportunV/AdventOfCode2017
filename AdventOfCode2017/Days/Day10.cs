6911f5a73ca3177d9ad7685bd8dd28f82f90f556
OpportunV <RsTGear@gmail.com>
2025-12-15 21:43:53 +0300

    Adjusted day 10 to use in day 14

--- AdventOfCode2017/Days/Day10.cs
+++ AdventOfCode2017/Days/Day10.cs
@@ -4,15 +4,15 @@ namespace AdventOfCode2017.Days;
 
 public class Day10 : Day
 {
-    private readonly List<int> _extraLengths = [17, 31, 73, 47, 23];
+    private static readonly List<byte> _extraLengths = [17, 31, 73, 47, 23];
     private const int ChunkSize = 16;
     private const int ListSize = 256;
     private const int RoundAmount = 64;
 
     public override object Part1()
     {
-        var lengths = GetInput()[0].GetNumbers<int>();
-        var list = Enumerable.Range(0, ListSize).ToList();
+        var lengths = GetInput()[0].GetNumbers<byte>();
+        var list = Enumerable.Range(0, ListSize).Select(i => (byte)i).ToList();
         var pos = 0;
         var skip = 0;
 
@@ -23,39 +23,44 @@ public class Day10 : Day
 
     public override object Part2()
     {
-        var lengths = GetInput()[0].Select(chr => (int)chr).ToList();
-        lengths.AddRange(_extraLengths);
-        var list = Enumerable.Range(0, ListSize).ToList();
+        var lengths = GetInput()[0].Select(chr => (byte)chr).ToList();
+        
+        return DenseHashHex(lengths);
+    }
+
+    public static string DenseHashHex(List<byte> data)
+    {
+        data.AddRange(_extraLengths);
+        var list = Enumerable.Range(0, ListSize).Select(i => (byte)i).ToList();
 
         var pos = 0;
         var skip = 0;
 
         for (var i = 0; i < RoundAmount; i++)
         {
-            DoRound(list, lengths, ref pos, ref skip);
+            DoRound(list, data, ref pos, ref skip);
         }
 
-        var denseHashHex = GetDenseHashHex(list);
-        return denseHashHex;
+        return GetDenseHashHex(list);
     }
 
-    private static string GetDenseHashHex(List<int> list)
+    private static string GetDenseHashHex(List<byte> list)
     {
         var denseHash = GetDenseHash(list);
         return Convert.ToHexStringLower(denseHash);
     }
 
-    private static byte[] GetDenseHash(List<int> list)
+    private static byte[] GetDenseHash(List<byte> list)
     {
         var denseHash = list
             .Chunk(ChunkSize)
-            .Select(chunk => (byte)chunk.Aggregate((acc, val) => acc ^ val))
+            .Select(chunk => chunk.Aggregate((acc, val) => (byte) (acc ^ val)))
             .ToArray();
 
         return denseHash;
     }
 
-    private static void DoRound(List<int> list, List<int> lengths, ref int pos, ref int skip)
+    private static void DoRound(List<byte> list, List<byte> lengths, ref int pos, ref int skip)
     {
         foreach (var length in lengths)
         {
@@ -67,7 +72,7 @@ public class Day10 : Day
         }
     }
 
-    private static void ReverseFragment(List<int> list, int start, int end)
+    private static void ReverseFragment(List<byte> list, int start, int end)
     {
         var length = list.Count;
         while (start < end)
