3684d5e41777b85f93d7800b24799f205b8021bd
OpportunV <RsTGear@gmail.com>
2025-12-15 21:44:03 +0300

    Day 14

--- AdventOfCode2017/Days/Day14.cs
+++ AdventOfCode2017/Days/Day14.cs
@@ -1,26 +1,70 @@
-using System.IO;
-using AdventOfCode2017.Helpers;
+using System.Text;
+using Common.Extensions;
+using Common.Models;
 
-namespace AdventOfCode2017.Days
+namespace AdventOfCode2017.Days;
+
+public class Day14 : Day
 {
-    public static class Day14
-    {
-        private static readonly string _inputPath = Path.Combine("input",
-            $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType?.Name}.txt");
-        
-        public static object Part1()
-        {
-            var lines = Helper.GetInput(_inputPath);
+    private readonly Grid<char> _grid;
+    private const int Amount = 128;
+    private const int Used = '1';
 
-            return -1;
-        }
-        
-        public static object Part2()
+    public Day14()
+    {
+        var key = GetInputRaw();
+        _grid = new Grid<char>(
+            Enumerable.Range(0, Amount)
+                .Select(i => Day10
+                    .DenseHashHex(Encoding.UTF8.GetBytes($"{key}-{i}").ToList())
+                    .GetHexBits()
+                    .ToArray())
+                .ToArray());
+    }
+
+    public override object Part1()
+    {
+        return _grid
+            .Flatten()
+            .Count(item => item.Value == Used);
+    }
+
+    public override object Part2()
+    {
+        var seen = new HashSet<GridPos2d>();
+        var res = 0;
+        foreach (var gridItem in _grid.Flatten())
         {
-            var lines = Helper.GetInput(_inputPath);
-            
-            return -1;
+            if (seen.Contains(gridItem.Pos))
+            {
+                continue;
+            }
+
+            if (gridItem.Value != Used)
+            {
+                continue;
+            }
+
+            res += 1;
+            var toVisit = new Queue<GridPos2d>();
+            toVisit.Enqueue(gridItem.Pos);
+            while (toVisit.TryDequeue(out var cur))
+            {
+                foreach (var next in _grid.AdjacentSide(cur))
+                {
+                    if (next.Value != Used)
+                    {
+                        continue;
+                    }
+
+                    if (seen.Add(next.Pos))
+                    {
+                        toVisit.Enqueue(next.Pos);
+                    }
+                }
+            }
         }
+
+        return res;
     }
 }
-
