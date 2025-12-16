33ff8ef56f5333b0336c627a555b7651f5e00ee4
OpportunV <RsTGear@gmail.com>
2025-12-16 23:33:30 +0300

    Day 17

--- AdventOfCode2017/Days/Day17.cs
+++ AdventOfCode2017/Days/Day17.cs
@@ -1,26 +1,47 @@
-using System.IO;
-using AdventOfCode2017.Helpers;
+namespace AdventOfCode2017.Days;
 
-namespace AdventOfCode2017.Days
+public class Day17 : Day
 {
-    public static class Day17
-    {
-        private static readonly string _inputPath = Path.Combine("input",
-            $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType?.Name}.txt");
-        
-        public static object Part1()
-        {
-            var lines = Helper.GetInput(_inputPath);
+    private readonly int _steps;
+    private const int Part1Amount = 2017;
+    private const int Part2Amount = 50_000_000;
 
-            return -1;
-        }
-        
-        public static object Part2()
+    public Day17()
+    {
+        _steps = int.Parse(GetInputRaw());
+    }
+
+    public override object Part1()
+    {
+        var vals = new List<int>(Part1Amount + 1) { 0 };
+        var target = 0;
+        for (var i = 1; i <= Part1Amount; i++)
         {
-            var lines = Helper.GetInput(_inputPath);
-            
-            return -1;
+            var next = (_steps + target) % vals.Count + 1;
+            vals.Insert(next, i);
+            target = next;
         }
+
+        return vals[target + 1];
+    }
+
+    public override object Part2()
+    {
+        var len = 1;
+        var target = 0;
+        var res = 0;
+        for (var i = 1; i <= Part2Amount; i++)
+        {
+            var next = (_steps + target) % len + 1;
+            if (next == 1)
+            {
+                res = i;
+            }
+
+            target = next;
+            len++;
+        }
+
+        return res;
     }
 }
-
