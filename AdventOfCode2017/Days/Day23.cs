9b592652947b7cb0941e78939f48c9274d724386
OpportunV <RsTGear@gmail.com>
2025-12-22 16:18:54 +0300

    Day 23

--- AdventOfCode2017/Days/Day23.cs
+++ AdventOfCode2017/Days/Day23.cs
@@ -1,26 +1,106 @@
-using System.IO;
-using AdventOfCode2017.Helpers;
+namespace AdventOfCode2017.Days;
 
-namespace AdventOfCode2017.Days
+public class Day23 : Day
 {
-    public static class Day23
-    {
-        private static readonly string _inputPath = Path.Combine("input",
-            $"{System.Reflection.MethodBase.GetCurrentMethod().DeclaringType?.Name}.txt");
-        
-        public static object Part1()
-        {
-            var lines = Helper.GetInput(_inputPath);
+    private readonly string[] _program;
+    private const string AvailableRegs = "abcdefgh";
 
-            return -1;
-        }
-        
-        public static object Part2()
+    public Day23()
+    {
+        _program = GetInput();
+    }
+
+    public override object Part1()
+    {
+        var cur = 0L;
+        var res = 0;
+        var regs = AvailableRegs.ToDictionary(reg => reg, _ => 0L);
+        while (cur < _program.Length)
         {
-            var lines = Helper.GetInput(_inputPath);
-            
-            return -1;
+            var split = _program[cur].Split();
+            switch (split[0])
+            {
+                case "set":
+                    var reg = split[1][0];
+                    regs[reg] = GetValue(split[2], regs);
+                    break;
+                case "sub":
+                    reg = split[1][0];
+                    regs[reg] -= GetValue(split[2], regs);
+                    break;
+                case "mul":
+                    reg = split[1][0];
+                    regs[reg] *= GetValue(split[2], regs);
+                    res++;
+                    break;
+                case "jnz":
+                    var val1 = GetValue(split[1], regs);
+                    if (val1 != 0)
+                    {
+                        cur += GetValue(split[2], regs);
+                        continue;
+                    }
+
+                    break;
+            }
+
+            cur++;
         }
+
+        return res;
+    }
+
+
+    public override object Part2()
+    {
+        var cur = 0L;
+        var regs = AvailableRegs.ToDictionary(reg => reg, _ => 0L);
+        regs['a'] = 1;
+        while (cur < 11)
+        {
+            var split = _program[cur].Split();
+            switch (split[0])
+            {
+                case "set":
+                    var reg = split[1][0];
+                    regs[reg] = GetValue(split[2], regs);
+                    break;
+                case "sub":
+                    reg = split[1][0];
+                    regs[reg] -= GetValue(split[2], regs);
+                    break;
+                case "mul":
+                    reg = split[1][0];
+                    regs[reg] *= GetValue(split[2], regs);
+                    break;
+                case "jnz":
+                    var val1 = GetValue(split[1], regs);
+                    if (val1 != 0)
+                    {
+                        cur += GetValue(split[2], regs);
+                        continue;
+                    }
+
+                    break;
+            }
+
+            cur++;
+        }
+
+        var res = 0;
+        for (var i = regs['b']; i <= regs['c']; i += 17)
+        {
+            if (Enumerable.Range(2, (int)Math.Sqrt(i)).Any(div => i % div == 0))
+            {
+                res++;
+            }
+        }
+
+        return res;
+    }
+
+    private static long GetValue(string value, Dictionary<char, long> regs)
+    {
+        return regs.TryGetValue(value[0], out var reg) ? reg : long.Parse(value);
     }
 }
-
--- AdventOfCode2017/Program.cs
+++ AdventOfCode2017/Program.cs
@@ -1,4 +1,4 @@
 ﻿using AdventOfCode2017;
 
 var solver = new Solver();
-solver.PrintDay(22);
+solver.PrintDay(23);
